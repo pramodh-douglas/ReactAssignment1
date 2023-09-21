7d76d64d9c5564fdd0c7034b1295e3143dda6cf3
Rajapakse <rajapaksea@student.douglascollege.ca>
2023-09-20 17:40:56 -0700

    Added reset functionality to Q1

--- as1Solution.js
+++ as1Solution.js
@@ -1,4 +1,5 @@
 /* QUESTION 1 */
+// fetch html elements
 let roundSelector = document.getElementById("playto");
 let plyrOneBtn = document.getElementById("p1Button");
 let plyrTwoBtn = document.getElementById("p2Button");
@@ -7,14 +8,16 @@ let resetBtn = document.getElementById("reset");
 let plyrOneDisplay = document.getElementById("p1Display");  
 let plyrTwoDisplay = document.getElementById("p2Display");  
 
+// global variables for player scores
 let plyrOneScore = 0;
 let plyrTwoScore = 0;
 
+// add event listeners to the button
 plyrOneBtn.addEventListener("click", updateScore);
-
 plyrTwoBtn.addEventListener("click", updateScore);
+resetBtn.addEventListener("click", resetGame);
 
-
+// function to update score
 function updateScore(e) {
     if (e.target.id == "p1Button") {
         plyrOneScore++;
@@ -35,4 +38,20 @@ function updateScore(e) {
             plyrTwoDisplay.style.color = "green";
         }
     }
+}
+
+// function for reset game
+function resetGame() {
+    plyrOneBtn.disabled = false;
+    plyrTwoBtn.disabled = false;
+
+    plyrOneScore = 0;
+    plyrTwoScore = 0;
+
+    plyrOneDisplay.innerHTML = plyrOneScore;
+    plyrTwoDisplay.innerHTML = plyrTwoScore;
+
+    plyrOneDisplay.removeAttribute("style");
+    plyrTwoDisplay.removeAttribute("style");
+
 }
